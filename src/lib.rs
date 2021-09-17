b016f128e571393ac1ce62e2a15817091c392187
Tip ten Brink <T.M.tenBrink@student.tudelft.nl>
2021-09-17 16:46:24 +0200

    Now only a single Python function for ndims

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,26 @@
 use ndarray_rand::rand_distr::Binomial;
 use ndarray_rand::rand::prelude::Distribution;
 use ndarray_rand::rand::thread_rng;
-use numpy::{IntoPyArray, PyArray3, PyReadonlyArray3, PyReadonlyArray2, PyArray2};
+use numpy::{IntoPyArray, PyReadonlyArrayDyn, PyArrayDyn};
 use pyo3::prelude::{pymodule, PyModule, PyResult, Python};
+
 
 #[pymodule]
 fn rustfrc(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
-    #[pyfn(m)]
-    fn binom_split_3d<'py>(py: Python<'py>, x: PyReadonlyArray3<'py, i32>) -> PyResult<&'py PyArray3<i32>> {
-        let mut x = x.as_array().to_owned();
-        x.par_mapv_inplace(|i| {
-            let mut rng = thread_rng();
-            Binomial::new(i as u64, 0.5).unwrap().sample(&mut rng) as i32
-        });
-        Ok(x.into_pyarray(py))
-    }
 
+    /// binom_split(a)
+    /// --
+    ///
+    /// Takes an image (np.ndarray with dtype i32) and splits every pixel value according to the
+    /// binomial distribution (n, p) with n = pixel value and p = 0.5. Returns a single, split image.
     #[pyfn(m)]
-    fn binom_split_2d<'py>(py: Python<'py>, x: PyReadonlyArray2<'py, i32>) -> PyResult<&'py PyArray2<i32>> {
-        let mut x = x.as_array().to_owned();
-        x.par_mapv_inplace(|i| {
+    fn binom_split<'py>(py: Python<'py>, a: PyReadonlyArrayDyn<'py, i32>) -> PyResult<&'py PyArrayDyn<i32>> {
+        let mut a = a.as_array().to_owned();
+        a.par_mapv_inplace(|i| {
             let mut rng = thread_rng();
             Binomial::new(i as u64, 0.5).unwrap().sample(&mut rng) as i32
         });
-        Ok(x.into_pyarray(py))
+        Ok(a.into_pyarray(py))
     }
     Ok(())
 }
--- test.py
+++ test.py
@@ -5,15 +5,14 @@ import time
 
 rng = np.random.default_rng()
 img = dip.ImageRead('Benton')(0)[:388, :388]
-x = np.array(img)
-#x3d = np.repeat(x[:, :, np.newaxis], 1000, axis=2)
-half_x = np.rint(x/2).astype(int)
+x = np.array(img).astype(np.int32)
+x = np.repeat(x[:, :, np.newaxis], 1000, axis=2)
 # half_x = np.rint(x/2).astype(int)
 # print(half_x)
 start = time.time_ns()
-#a = rng.binomial(half_x, 0.5)
-a = r.binom_split_2d(half_x)
-b = half_x - a
+# a = rng.binomial(x, 0.5)
+a = r.binom_split(x)
+b = x - a
 # for i in range(100):
 #     a = rng.binomial(half_x, 0.5)
 #     b = half_x - a
