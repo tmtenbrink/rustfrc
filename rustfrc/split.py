02342a9af664ccb465de6cb8cb7b3aa873c39435
Tip ten Brink <T.M.tenBrink@student.tudelft.nl>
2021-09-19 23:45:41 +0200

    Version bump to 1.0.0, since package structure should not change too much and deployment is somewhat streamlined now.

--- rustfrc/split.py
+++ rustfrc/split.py
@@ -3,7 +3,8 @@ from .rustfrc import _internal
 
 
 def binom_split(a: np.ndarray) -> np.ndarray:
-    """Takes an image (some) and splits every pixel value according to the
+    """Takes an image (numpy array) and splits every pixel (element) value according to the
     binomial distribution (n, p) with n = pixel value and p = 0.5. Returns a single image.
+    This conserves shot noise.
     """
     return _internal.binom_split(a.astype(np.int32))
